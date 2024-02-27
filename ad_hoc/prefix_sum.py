80ba023cc117b0fc963610d23f7bc3f992c53885
IMingGarson <92239878+IMingGarson@users.noreply.github.com>
2024-02-27 00:56:55 -0600

    Update prefix_sum.py

--- ad_hoc/prefix_sum.py
+++ ad_hoc/prefix_sum.py
@@ -1,5 +1,5 @@
 def prefix_sum(nums):
-    prefix_sum_list = [0] * (len(nums) + 1) 
-    for i in range(1, len(nums) + 1):
-        prefix_sum_list[i] = prefix_sum_list[i - 1] + nums[i - 1]
+    prefix_sum_list = [nums[0]]
+    for i in range(1, n):
+        prefix_sum_list.append(prefix_sum_list[-1] + nums[i])
     return prefix_sum_list
