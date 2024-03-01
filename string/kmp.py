52d51d4fda9ad8461d5efd6e996c5922872bd2b4
IMingGarson <92239878+IMingGarson@users.noreply.github.com>
2024-03-01 03:08:12 -0600

    Update kmp.py

--- string/kmp.py
+++ string/kmp.py
@@ -1,3 +1,5 @@
+# 3008. Find Beautiful Indices in the Given Array II 
+# https://leetcode.com/problems/find-beautiful-indices-in-the-given-array-ii/description/
 def kmp(search, target):
   ret = []
   if not target:
