1c0ce6deee6c588e8023bcaa9687481c3c87566e
IMingGarson <92239878+IMingGarson@users.noreply.github.com>
2024-03-03 21:00:46 -0600

    Create fenwick_tree.py

---
+++ tree/fenwick_tree.py
@@ -0,0 +1,19 @@
+class FenwickTree:
+  def __init__(self, n):
+      self.size = n
+      self.tree = [0] * (n + 1)
+
+  def update(self, index, delta):
+      while index <= self.size:
+          self.tree[index] += delta
+          index += (index & -index)
+
+  def prefix_sum(self, index):
+      result = 0
+      while index > 0:
+          result += self.tree[index]
+          index -= (index & -index)
+      return result
+
+  def range_query(self, start, end):
+      return self.prefix_sum(end) - self.prefix_sum(start - 1)
