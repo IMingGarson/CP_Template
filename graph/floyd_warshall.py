41e34ecb9ab5f3dfb314a6b8e6fad11014eadde7
IMingGarson <92239878+IMingGarson@users.noreply.github.com>
2024-03-04 19:42:48 -0600

    Create floyd_warshall.py

---
+++ graph/floyd_warshall.py
@@ -0,0 +1,20 @@
+def floyd_warshall(graph):
+  V = len(graph)
+  dist = [[float('inf')] * V for _ in range(V)]
+
+  for i in range(V):
+      dist[i][i] = 0
+    
+  for i in range(V):
+      for j in range(V):
+          if graph[i][j] != INF:
+              dist[i][j] = graph[i][j]
+
+  # Main algorithm
+  for k in range(V):
+      for i in range(V):
+          for j in range(V):
+              if dist[i][k] + dist[k][j] < dist[i][j]:
+                  dist[i][j] = dist[i][k] + dist[k][j]
+
+  return dist
