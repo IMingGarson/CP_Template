2832db9de8f7f0d124c19ac8ca095036dacb9357
IMingGarson <92239878+IMingGarson@users.noreply.github.com>
2024-03-07 01:46:27 -0600

    Update bfs.py

--- graph/bfs.py
+++ graph/bfs.py
@@ -1,3 +1,9 @@
+# https://leetcode.com/problems/flood-fill/description/
+# https://leetcode.com/problems/number-of-islands/
+# https://leetcode.com/problems/pacific-atlantic-water-flow/description/
+# Fun variant: https://leetcode.com/problems/island-perimeter/
+# depending on where you can go, directions may vary
+
 m, n = len(grid), len(grid[0])
 directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
