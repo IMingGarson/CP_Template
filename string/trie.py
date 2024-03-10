fac1a4ef0d566a9e7ad18ba18d721f1492a00609
IMingGarson <92239878+IMingGarson@users.noreply.github.com>
2024-03-10 23:08:43 +0800

    Update trie.py

--- string/trie.py
+++ string/trie.py
@@ -1,45 +1,45 @@
 class TrieNode:
-  def __init__(self):
-      self.children = {}
-      self.is_end_of_word = False
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
 
 class Trie:
-  def __init__(self):
-      self.root = TrieNode()
+    def __init__(self):
+        self.root = TrieNode()
 
-  def insert(self, word):
-      current_node = self.root
-      for char in word:
-          if char not in current_node.children:
-              current_node.children[char] = TrieNode()
-          current_node = current_node.children[char]
-      current_node.is_end_of_word = True
+    def insert(self, word):
+        current_node = self.root
+        for char in word:
+            if char not in current_node.children:
+                current_node.children[char] = TrieNode()
+            current_node = current_node.children[char]
+        current_node.is_end_of_word = True
 
-  def search(self, word):
-      current_node = self.root
-      for char in word:
-          if char not in current_node.children:
-              return False
-          current_node = current_node.children[char]
-      return current_node.is_end_of_word
+    def search(self, word):
+        current_node = self.root
+        for char in word:
+            if char not in current_node.children:
+                return False
+            current_node = current_node.children[char]
+        return current_node.is_end_of_word
 
-  def starts_with(self, prefix):
-      current_node = self.root
-      for char in prefix:
-          if char not in current_node.children:
-              return False
-          current_node = current_node.children[char]
-      return True
-    
-  # This calculate the longest possible prefix of a string to this trie
-  # https://leetcode.com/problems/find-the-length-of-the-longest-common-prefix/description/
-  def longest_prefix(self, word):
-      current_node = self.root
-      prefix = ""
-      for char in word:
-          if char not in current_node.children:
-              break
-          prefix += char
-          current_node = current_node.children[char]
-      return prefix # or len(prefix) for length
-  
+    def starts_with(self, prefix):
+        current_node = self.root
+        for char in prefix:
+            if char not in current_node.children:
+                return False
+            current_node = current_node.children[char]
+        return True
+
+    # This calculate the longest possible prefix of a string to this trie
+    # https://leetcode.com/problems/find-the-length-of-the-longest-common-prefix/description/
+    def longest_prefix(self, word):
+        current_node = self.root
+        prefix = ""
+        for char in word:
+            if char not in current_node.children:
+                break
+            prefix += char
+            current_node = current_node.children[char]
+        return prefix  # or len(prefix) for length
