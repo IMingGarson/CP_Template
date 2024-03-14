3fda89f370153117764379c599dc0fda16ab39b9
IMingGarson <92239878+IMingGarson@users.noreply.github.com>
2024-03-13 21:31:49 -0500

    Update prefix_sum.py

--- ad_hoc/prefix_sum.py
+++ ad_hoc/prefix_sum.py
@@ -4,3 +4,9 @@ def prefix_sum(nums):
     for i in range(1, n):
         prefix_sum_list.append(prefix_sum_list[-1] + nums[i])
     return prefix_sum_list
+
+# More Pythonic
+# https://leetcode.com/problems/find-the-pivot-integer/description/
+n = len(nums)
+prefix_sum_list = list(accumulate(range(1,n + 1),lambda x, y : x + y))
+suffix_sum_list = list(accumulate(range(1,n + 1)[::-1],lambda x, y : x + y))[::-1]
