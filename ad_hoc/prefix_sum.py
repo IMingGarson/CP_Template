69409fd2dcf79bae5f04f0ba7ceb46986184db65
IMingGarson <92239878+IMingGarson@users.noreply.github.com>
2024-03-16 11:30:11 +0800

    Update prefix_sum.py

--- ad_hoc/prefix_sum.py
+++ ad_hoc/prefix_sum.py
@@ -8,8 +8,6 @@ def prefix_sum(nums):
 # More Pythonic
 # https://leetcode.com/problems/find-the-pivot-integer/description/
 
-# NOTE: itertools.accumulate won't work on negative numbers
 # https://leetcode.com/problems/range-sum-query-immutable/
-n = len(nums)
-prefix_sum_list = list(accumulate(range(1,n + 1),lambda x, y : x + y))
-suffix_sum_list = list(accumulate(range(1,n + 1)[::-1],lambda x, y : x + y))[::-1]
+prefix_sum_list = list(accumulate(nums, lambda x, y : x + y))
+suffix_sum_list = list(accumulate(nums[::-1], lambda x, y : x + y))[::-1]
