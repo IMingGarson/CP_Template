0a379a15f7d9065af636a561e8785dee09f14728
IMingGarson <92239878+IMingGarson@users.noreply.github.com>
2024-03-17 22:54:15 -0500

    Create kadane.py

---
+++ ad_hoc/kadane.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/maximum-subarray/
+def kadane_max_subarray_sum(nums):
+	n = len(nums)
+    max_current = max_global = nums[0]
+
+    for i in range(1, n):
+        max_current = max(nums[i], max_current + nums[i])
+        if max_current > max_global:
+            max_global = max_current
+
+    return max_global
+
+# https://leetcode.com/problems/maximum-product-subarray/
+def kadane_max_subarray_product(nums):
+	n = len(nums)
+	max_so_far, max_ending, min_ending = nums[0], nums[0], nums[0]
+	for i in range(1, n):
+		current = max(
+			nums[i],
+			max_ending * nums[i],
+			min_ending * nums[i]
+		)
+		min_ending = min(
+			nums[i],
+			max_ending * nums[i],
+			min_ending * nums[i]
+		)
+		max_ending = current
+		max_so_far = max(max_so_far, max_ending)
+	return max_so_far
