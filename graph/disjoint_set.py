b006dde453cb3d61c3aadbcdee0b7bd6e240fff1
IMingGarson <92239878+IMingGarson@users.noreply.github.com>
2024-03-17 23:45:15 -0500

    Update disjoint_set.py

--- graph/disjoint_set.py
+++ graph/disjoint_set.py
@@ -1,30 +1,29 @@
 # https://leetcode.com/problems/number-of-provinces/
 class DisjointSet:
-  def __init__(self, n):
-      self.root = [i for i in range(n)]
-      self.rank = [1] * n
-  
-  def find(self, x):
-      parent = x
-      while parent != self.root[parent]:
-          self.root[parent] = self.root[self.root[parent]]
-          parent = self.root[parent]
-      return parent
+    def __init__(self, n):
+        self.root = [i for i in range(n)]
+        self.rank = [1] * n
 
-  def union(self, x, y):
-      root_x = self.find(x)
-      root_y = self.find(y)
-      if root_x == root_y:
-          return 0
-      if root_x != root_y:
-          if self.rank[root_x] > self.rank[root_y]:
-              self.rank[root_y] = root_x
-              self.rank[root_x] += self.rank[root_y]
-          else:
-              self.rank[root_x] = root_y
-              self.rank[root_y] += self.rank[root_x]
-      return 1
-  
-  def is_connected(self, x, y):
-      return self.root[x] == self.root[y]
-    
+    def find(self, x):
+        parent = x
+        while parent != self.root[parent]:
+            self.root[parent] = self.root[self.root[parent]]
+            parent = self.root[parent]
+        return parent
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x == root_y:
+            return 0
+        if root_x != root_y:
+            if self.rank[root_x] > self.rank[root_y]:
+                self.rank[root_y] = root_x
+                self.rank[root_x] += self.rank[root_y]
+            else:
+                self.rank[root_x] = root_y
+                self.rank[root_y] += self.rank[root_x]
+        return 1
+
+    def is_connected(self, x, y):
+        return self.root[x] == self.root[y]
