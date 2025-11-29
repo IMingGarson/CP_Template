c8fb48f84d56d0a63e2a61378efd543fac23cdce
IMingGarson <92239878+IMingGarson@users.noreply.github.com>
2025-11-29 11:11:29 +0800

    Refactor FenwickTree to Fenwick with new methods

--- tree/fenwick_tree.py
+++ tree/fenwick_tree.py
@@ -1,37 +1,28 @@
 # Fenwick Tree can get range sum in log(n) time for mutable input array
 # https://leetcode.com/problems/range-sum-query-mutable/
-class FenwickTree:
-    def __init__(self, nums):
-        self.size = len(nums)
-        self.nums = nums
-        self.tree = [0 for _ in range(self.size + 1)]
-        for i in range(self.size):
-            self.initialize(i, nums[i])
+class Fenwick:
+    def __init__(self, n: int):
+        self.n = n
+        self.bit = [0] * (n + 1)
 
-    def initialize(self, index: int, val: int) -> None:
-        # binary indexed trees start at index 1
-        index += 1
-        while index <= self.size:
-            self.tree[index] += val
-            index += index & -index
+    def add(self, i: int, delta: int) -> None:
+        """a[i] += delta"""
+        i += 1
+        while i <= self.n:
+            self.bit[i] += delta
+            i += i & -i
 
-    def update(self, index, delta):
-        prev_val = self.nums[index]
-        self.nums[index] = delta
-        index += 1
-        while index <= self.size:
-            self.tree[index] += delta - prev_val
-            index += index & -index
-
-    def prefix_sum(self, index):
-        if index < 0:
+    def sum(self, i: int) -> int:
+        """prefix sum [0..i]，i < 0 returns 0"""
+        if i < 0:
             return 0
-        result = 0
-        index += 1
-        while index > 0:
-            result += self.tree[index]
-            index -= index & -index
-        return result
+        i += 1
+        res = 0
+        while i > 0:
+            res += self.bit[i]
+            i -= i & -i
+        return res
 
-    def range_query(self, start, end):
-        return self.prefix_sum(end) - self.prefix_sum(start - 1)
+    def range_sum(self, l: int, r: int) -> int:
+        """sum over [l..r]"""
+        return self.sum(r) - self.sum(l - 1)
